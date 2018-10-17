243a195afd6686714c1178795e7dfa975b31c25e
geoffhanna <geoff@hannasolutions.com>
2018-10-16 19:16:54 -0600

    start again

---
+++ filerLibrary/IPaper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace filerLibrary
+{
+    interface IPaper
+    {
+        string Name { get; set; }
+        string Folder{ get; set; }
+        string HangingFolder { get; set; }
+        string HangingFolderGroup { get; set; }
+        string Drawer { get; set; }
+    }
+}
