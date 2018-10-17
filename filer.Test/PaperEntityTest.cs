cbde21613ed65569e1b9b1f7d093abe08be843ec
geoffhanna <geoff@hannasolutions.com>
2018-10-16 18:44:37 -0600

    First PoC work

---
+++ filer.Test/PaperEntityTest.cs
@@ -0,0 +1,18 @@
+﻿using filerLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace filer.Test
+{
+    [TestClass]
+    public class PaperEntityTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var paper = new PaperEntity();
+            paper.Name = "geoff";
+            paper.HangingFolderGroup = "x";
+            var x = paper.RowKey;
+        }
+    }
+}
---
+++ filerLibrary/PaperEntity.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+
+namespace filerLibrary
+{
+    public class PaperEntity : TableEntity, IPaper
+    {
+
+        public PaperEntity ()
+        {
+            this.PartitionKey = "Paper";
+            Name = string.Empty;
+            Folder = string.Empty;
+            HangingFolder = string.Empty;
+            HangingFolderGroup = string.Empty;
+            Drawer = string.Empty;
+        }
+        public string Name { get; set; }
+        public string Folder { get; set; }
+        public string HangingFolder { get; set; }
+        public string HangingFolderGroup { get; set; }
+        public string Drawer { get; set; }
+        public new string RowKey
+        {
+            get
+            {
+                return $"{Name.GetHashCode().ToString("X8")}{Folder.GetHashCode().ToString("X8")}{HangingFolder.GetHashCode().ToString("X8")}{HangingFolderGroup.GetHashCode().ToString("X8")}{Drawer.GetHashCode().ToString("X8")}";
+            }
+            private set { }
+        }
+    }
+}
